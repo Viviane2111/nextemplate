dee97a2d0b383fe77dc739bcec6bf0c6550a8519
Viviane <arrghe.dev@gmail.com>
2024-05-19 09:58:03 +0200

    adding tailwind

--- components/Home.js
+++ components/Home.js
@@ -1,11 +1,15 @@
-import styles from "../styles/Home.module.css";
-
 export default function Home() {
   return (
     <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+      <main className="custom-ubuntu-font flex flex-col justify-center items-center min-h-screen py-16">
+        <h1 className="text-6xl md:text-6xl lg:text-7xl font-semibold leading-normal">
+          Welcome to{" "}
+          <a
+            className="text-blue-500 hover:underline"
+            href="https://nextjs.org"
+          >
+            Next.js!
+          </a>
         </h1>
       </main>
     </div>
